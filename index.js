2de1530557b1890da90582401702471d23784316
anusayabuchade <anusayabuchade5601@gmail.com>
2025-08-14 22:10:49 +0530

    add functions

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const display = document.getElementById("display");
+const lapsContainer = document.getElementById("laps");
+
 let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
+let lapCount = 0;
 
 function start(){
-    if(isRunning){
+    if(!isRunning){
         startTime = Date.now() - elapsedTime;
         timer = setInterval(update, 10);
         isRunning = true;
+    }
+}
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime ;
+        isRunning = false;
+    }
+}
+
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+}
+
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) %6);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 /10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
+
+function lap() {
+    if (isRunning) {
+        const lapTime = display.textContent;
+        const li = document.createElement("li");
+        li.textContent = lapTime;
+        lapsContainer.appendChild(li);
     }
 }
